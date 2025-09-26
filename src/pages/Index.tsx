8a4c0fa4f5c59157a46593cd768f289ae1e80011
Akula Rentals <user@example.com>
2025-09-26 16:47:46 +0530

    fixxed

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -343,7 +343,7 @@ const Index = () => {
         )}
 
         {appState === 'addons' && checkoutSteps.length > 0 && (
-          <div className="w-full h-full overflow-auto bg-white" style={{ minHeight: '120vh' }}>
+          <div className="w-full h-full overflow-auto bg-white" style={{ minHeight: '135vh' }}>
             <div className="max-w-5xl mx-auto p-4">
               <style dangerouslySetInnerHTML={{
                 __html: `
